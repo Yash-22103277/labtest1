2c6858bdb1075bfcc3cf116a57d653514728ca77
Yash Gautam <22103277@mail.jiit.ac.in>
2024-09-21 10:05:08 +0530

    Add files via upload

---
+++ labtest1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+current = np.array([])
+
+a = ([{'name', str}, {'quantity', int}])
+
+item = np.array([{'yash', 4}, {'harsh', 10}, {'manas', 9}])
+
+for i in current:
+    ind = np.where(item['id' == i])
+    print(ind)
+    item[ind]['quantity']
+    print(item[ind]['quantity'])
+
+print(item)
+print(a)
